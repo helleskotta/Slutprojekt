736c3dca39d3cb0568ce01650e2f783832c5abd4
Helen Luu <helleskotta@gmail.com>
2017-09-04 10:27:56 +0200

    Added MVC-project

---
+++ SlutprojektBackend/Models/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlutprojektBackend.Models
+{
+    public class Class
+    {
+        // Alla proppar
+    }
+}
---
+++ SlutprojektBackend/Models/DataManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SlutprojektBackend.Models
+{
+    public class DataManager
+    {
+        // Listor, data och metoder för att hantera datan
+    }
+}
