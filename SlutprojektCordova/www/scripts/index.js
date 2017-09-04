57dab71b5ad93652b0e49c4d5c161c6ab8bf41c2
Helen Luu <helleskotta@gmail.com>
2017-09-04 15:56:21 +0200

    Made login page, added jQuery files and logo, added a test-page and onclick-function in js-file

--- SlutprojektCordova/www/scripts/index.js
+++ SlutprojektCordova/www/scripts/index.js
@@ -1,7 +1,6 @@
-﻿// For an introduction to the Blank template, see the following documentation:
-// http://go.microsoft.com/fwlink/?LinkID=397704
-// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
+﻿// To debug code on page load in cordova-simulate or on Android devices/emulators: launch your app, set breakpoints, 
 // and then run "window.location.reload()" in the JavaScript Console.
+
 (function () {
     "use strict";
 
@@ -12,12 +11,12 @@
         document.addEventListener( 'pause', onPause.bind( this ), false );
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //// TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+        //var parentElement = document.getElementById('deviceready');
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
     };
 
     function onPause() {
@@ -27,4 +26,12 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here.
     };
-} )();
+})();
+
+function Redirect() {
+    window.location = "page.html";
+}
+
+$("#redirect").click(function () {
+    window.location = "page.html";
+});
