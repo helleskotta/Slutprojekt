e004ad720fe1cc0988d22e16dab885c52c0e355b
Daniel <dannelinder21@gmail.com>
2017-09-05 21:04:22 +0200

    Added some body stats

--- SlutprojektBackend/Models/ViewModels/StatisicsVM.cs
+++ SlutprojektBackend/Models/ViewModels/StatisicsVM.cs
@@ -10,11 +10,15 @@ namespace SlutprojektBackend.Models.ViewModels
         public string TypeOfWorkoutSession { get; set; }
         public string TypeOfExercise { get; set; }
         public float BodyWeight { get; set; }
-        public float GainedWeight { get; set; }
-        public int Period { get; set; }
+        public float Waist { get; set; }
+        public float Chest { get; set; }
+        public float Hips { get; set; }
+        public float LiftedWeight { get; set; }
+        public DateTime Date { get; set; }
         //public List<BodyStats> BodyStats;
         //public List<ProgressStats> ProgressStats;
         public int ActiveStreak { get; set; }
+        public int MyProperty { get; set; }
 
     }
 }
