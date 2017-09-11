3158a4b146eb3078e50c895b480085eeb0d3de80
Helen Luu <helleskotta@gmail.com>
2017-09-11 21:42:14 +0200

    Changed displayed dated, added styling and other fun stuff

--- SlutprojektCordova/www/scripts/calendar.js
+++ SlutprojektCordova/www/scripts/calendar.js
@@ -1,6 +1,8 @@
 ﻿var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+var days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 
-document.addEventListener("deviceready", function() {
+
+document.addEventListener("deviceready", function () {
 
     $("#homeicon").removeClass("selectedicon");
     $("#calendaricon").addClass("selectedicon");
@@ -11,11 +13,38 @@ document.addEventListener("deviceready", function() {
     var listviewfull = "";
 
     for (var i = allWorkoutSessions.length - 1; i > 0; i--) {
-        var displayDate = new Date(allWorkoutSessions[i].date).getDate();
-        var displayMonth = months[new Date(allWorkoutSessions[i].date).getMonth()];
-        var displayDM = displayDate + " " + displayMonth;
+        var displayDate = new Date(allWorkoutSessions[i].date).getDate() + " " + months[new Date(allWorkoutSessions[i].date).getMonth()];
 
-        listviewfull += '<li><span class="listdate">' + displayDM + '</span><span class="listcontent">' + allWorkoutSessions[i].sessionName + '</span></li>'
+        var weekday = days[new Date(allWorkoutSessions[i].date).getDay()];
+
+        var today = new Date().getDate() + " " + months[new Date().getMonth()];
+
+        var sessionName = allWorkoutSessions[i].sessionName;
+        var icon = '<br /><br /><img align="right" style="width:30px; padding-top:30px; padding-right:15px;" src="';
+        var duration = allWorkoutSessions[i].duration;
+
+        if (allWorkoutSessions[i].type === "Strength") {
+            icon += 'images/strengthicon.png" />';
+        }
+
+        else if (allWorkoutSessions[i].type === "Cardio") {
+            icon += 'images/cardioicon.png" />';
+            sessionName += " " + duration + " min";
+        }
+
+        else {
+            icon += 'images/othericon.png" />';
+        }
+
+        if (displayDate === today) {
+            weekday = "today";
+            listviewfull += '<span>' + icon + weekday +  '</span><li style="border-left:2px solid green;">';
+        }
+        else {
+            listviewfull += '<span>' + icon + weekday + '</span><li>';
+        }
+
+        listviewfull += '<span class="listdate">' + displayDate + '</span><span class="listcontent">' + sessionName + '</span></li>';
     }
     $("#calList").append(listviewfull);
 });
