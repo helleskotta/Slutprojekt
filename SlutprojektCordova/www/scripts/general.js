997ab705c39e6df88796863cd12eb0308371672f
Petter Olgemar <petterolgemar_92@hotmail.com>
2017-09-13 11:17:55 +0200

    Backend working

--- SlutprojektCordova/www/scripts/general.js
+++ SlutprojektCordova/www/scripts/general.js
@@ -1,7 +1,7 @@
 ﻿var storage = window.localStorage;
 var localDomain = "http://localhost:49902";
 var azureDomain = "http://slutprojektbackend.azurewebsites.net";
-var currentDomain = localDomain;
+var currentDomain = azureDomain;
 
 document.addEventListener("deviceready", function () {
 
